88e2adbb825350c2b33c3105d564dbd3c6beaf3f
Deni Bertovic <deni@kset.org>
2011-10-09 15:32:52 +0000

    changed js functions to work with lightbox

--- media/frontend/js/custom-functions.js
+++ media/frontend/js/custom-functions.js
@@ -7,36 +7,36 @@ var current_event = 0;
 // 
 
 // on page load
-$(document).ready( function()
+jQuery(document).ready( function()
     {
 	// top menu prompt blink
 	window.setInterval(function(){
-	    $('#top-menu-prompt').toggleClass('prompt-blink');
+	    jQuery('#top-menu-prompt').toggleClass('prompt-blink');
 	}, 500);
 	
-	events_count = $('#header-scroll > div').length;
+	events_count = jQuery('#header-scroll > div').length;
 	
 	// bind events
-	$('#newsletter-subscription').click(function () { $('#newsletter-subscription').val(''); });
-	$('#newsletter-submit').click( function () 
+	jQuery('#newsletter-subscription').click(function () { $('#newsletter-subscription').val(''); });
+	jQuery('#newsletter-submit').click( function () 
 				       { 
-					   $.post(
+					   jQuery.post(
 					       "/subscribe/",
-					       { subscription: $('#newsletter-subscription').val()},
+					       { subscription: jQuery('#newsletter-subscription').val()},
 					       function (data) { 
-						   $('#newsletter-message').html(data);
-						   $('#newsletter-message').show();
+						   jQuery('#newsletter-message').html(data);
+						   jQuery('#newsletter-message').show();
 					       }
 					   ); 
 				       });
 	// da enter bude isto sto i ok
-        $('#form-newsletter').submit( function (event){
-					$.post(
+        jQuery('#form-newsletter').submit( function (event){
+					jQuery.post(
 					       "/subscribe/",
-					       { subscription: $('#newsletter-subscription').val()},
+					       { subscription: jQuery('#newsletter-subscription').val()},
 					       function (data) { 
-						   $('#newsletter-message').html(data);
-						   $('#newsletter-message').show();
+						   jQuery('#newsletter-message').html(data);
+						   jQuery('#newsletter-message').show();
 					       }
 					   ); 
 					event.preventDefault();
@@ -49,17 +49,17 @@ $(document).ready( function()
 function header_scroll_down()
 {
     if (current_event < events_count - max_events)
-	$('#event-' + current_event++).animate({opacity:0},200).animate({height:'toggle'},300);
+	jQuery('#event-' + current_event++).animate({opacity:0},200).animate({height:'toggle'},300);
 }
 
 function header_scroll_up()
 {
     if (current_event > 0)
-	$('#event-' + --current_event).animate({height:'toggle'},300).animate({opacity:100},200);
+	jQuery('#event-' + --current_event).animate({height:'toggle'},300).animate({opacity:100},200);
 }
 
 function flip_element_by_id(id) {
-    $('#'+id).animate({height:'toggle'}, 300);
+    jQuery('#'+id).animate({height:'toggle'}, 300);
 };
 
 
