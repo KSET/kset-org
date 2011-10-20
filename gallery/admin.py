617e05a2f5d2f3481fd90a78b43b004394c426d8
Deni Bertovic <deni@kset.org>
2011-10-20 11:08:19 +0000

    Removed object based permissions from gallery admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -34,32 +34,32 @@ class AlbumAdmin(GuardedModelAdmin):
     search_fields = ('title','date_of_event','description')
 
 
-    def change_view(self, request, object_id, extra_context=None):
-      joe = User.objects.get(username=request.user)
-      obj = self.get_object(request, unquote(object_id))
-      site = Site.objects.get_current()
-      if (joe.has_perm("change_album", obj)):
-        #messages.add_message(request, messages.ERROR, u"You don't have the necessary permissions!")
-        #return HttpResponse(request.path)
-        return super(AlbumAdmin, self).change_view(request, object_id, extra_context=None)
-      else:
-        self.message_user(request, "Nemate ovlasti za editiranje ovog albuma!")
-        return HttpResponseRedirect("/admin/gallery/album/")
-
-    #TODO - delete_view
-
-    def queryset(self, request):
-      """
-      Filter the objects displayed in the change_list to only
-      display those for the currently signed in user.
-      """
-      qs = super(AlbumAdmin, self).queryset(request)
-      if request.user.is_superuser:
-        return qs
-      else:
-        #return qs.filter(owner=request.user)
-        qs = get_objects_for_user(request.user, 'view_album', Album.objects.all())
-        return qs
+#    def change_view(self, request, object_id, extra_context=None):
+#      joe = User.objects.get(username=request.user)
+#      obj = self.get_object(request, unquote(object_id))
+#      site = Site.objects.get_current()
+#      if (joe.has_perm("change_album", obj)):
+#        #messages.add_message(request, messages.ERROR, u"You don't have the necessary permissions!")
+#        #return HttpResponse(request.path)
+#        return super(AlbumAdmin, self).change_view(request, object_id, extra_context=None)
+#      else:
+#        self.message_user(request, "Nemate ovlasti za editiranje ovog albuma!")
+#        return HttpResponseRedirect("/admin/gallery/album/")
+#
+#    #TODO - delete_view
+#
+#    def queryset(self, request):
+#      """
+#      Filter the objects displayed in the change_list to only
+#      display those for the currently signed in user.
+#      """
+#      qs = super(AlbumAdmin, self).queryset(request)
+#      if request.user.is_superuser:
+#        return qs
+#      else:
+#        #return qs.filter(owner=request.user)
+#        qs = get_objects_for_user(request.user, 'view_album', Album.objects.all())
+#        return qs
 
 
 
