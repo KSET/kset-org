c1c39e787fbe4428d2fe65b38346ca6257965351
Deni Bertovic <deni@kset.org>
2013-11-02 19:53:54 +0100

    added newsletter initial migrations

---
+++ newsletter/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Subscription'
+        db.create_table(u'newsletter_subscription', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=75)),
+        ))
+        db.send_create_signal(u'newsletter', ['Subscription'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Subscription'
+        db.delete_table(u'newsletter_subscription')
+
+
+    models = {
+        u'newsletter.subscription': {
+            'Meta': {'object_name': 'Subscription'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['newsletter']
