40cf77b2be4af7afdddbe14bd0743fa925dcf906
Deni Bertovic <deni@kset.org>
2013-11-16 17:54:20 +0100

    ctx does some weird stuff, try to fix
    A non optimal way to atleast stop it from crashing and
    burning.

--- ctx.py
+++ ctx.py
@@ -21,8 +21,11 @@ def header(request):
     ctx['header_events'] = Event.objects.filter(date__gte=datetime.now()).order_by('date')
 
     if request.path[1:].split('/')[0] == 'klub':
-        ctx['divisions'] = Subpage.objects.filter(category__slug='sekcije').order_by('title')
-        ctx['leaders'] = Subpage.objects.get(slug='voditelji')
-        ctx['projects'] = Subpage.objects.filter(category__slug='projekti').order_by('title')
+        try:
+            ctx['divisions'] = Subpage.objects.filter(category__slug='sekcije').order_by('title')
+            ctx['leaders'] = Subpage.objects.get(slug='voditelji')
+            ctx['projects'] = Subpage.objects.filter(category__slug='projekti').order_by('title')
+        except:
+            pass
 
     return ctx
