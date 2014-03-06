eb12c054a7514ecda4e2d4688a0e57aac92def3e
Deni Bertovic <deni@kset.org>
2014-03-06 17:20:57 +0100

    add data migration to lower all existing event tags

---
+++ events/migrations/0006_make_sure_to_lower_existing_tags.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+
+class Migration(DataMigration):
+    def forwards(self, orm):
+        events = orm.Event.objects.all()
+        for event in events:
+            event.tags = [t.strip().lower() for t in event.tags]
+            event.save()
+
+    def backwards(self, orm):
+        raise RuntimeError("Cannot reverse this migration.")
+
+    models = {
+        u'events.event': {
+            'Meta': {'object_name': 'Event'},
+            'announce': ('django.db.models.fields.BooleanField', [], {}),
+            'content': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'daytime': ('django.db.models.fields.BooleanField', [], {}),
+            'description': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.CharField', [], {'max_length': '16', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '128', 'blank': 'True'}),
+            'tags': ('djorm_pgarray.fields.ArrayField', [], {'default': 'None', 'dbtype': "'text'", 'null': 'True', 'blank': 'True'}),
+            'thumb': ('filebrowser.fields.FileBrowseField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '192'})
+        }
+    }
+
+    complete_apps = ['events']
+    symmetrical = True
