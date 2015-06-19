4315697a865734d1231802b91f89ead5e29629fb
Belma Gutlic <belma.dzi@gmail.com>
2015-06-19 09:12:06 +0200

    Strip non-digits from Facebook ID field

--- events/forms.py
+++ events/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from .models import Event
 
+import re
+
 
 class EventAdminForm(forms.ModelForm):
     class Meta:
@@ -17,3 +19,8 @@ class EventAdminForm(forms.ModelForm):
                 cleaned_tags.append(tag.strip().lower())
 
         return cleaned_tags
+
+    def clean_fbeventid(self):
+        fbeventid = self.cleaned_data.get('fbeventid')
+
+        return  re.sub("[^0-9]", "", fbeventid)
